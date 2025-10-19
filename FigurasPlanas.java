89758271f64cd25a526735a12404ea54ea98bd15
Paulo <135683232+paulo-alt@users.noreply.github.com>
2025-10-19 18:15:50 -0300

    Add files via upload

---
+++ FigurasPlanas.java
@@ -0,0 +1,16 @@
+package figurasplanas:
+
+public class quadrado {
+    
+    public quadrado(double lado) {
+        this.lado = lado;
+    }
+    public double area(){
+        return lado*lado;
+    }
+    @Override
+    public String toString(){
+        return String.format ("Quadrado\n Lado:%.2f \n Área:%.2f\n", lado area()
+        
+    }
+}
